911200bac319acaada16191274a3cd28331b9d8e
Newton Galiza <newtonjgaliza@gmail.com>
2017-03-28 15:17:35 -0300

    Update ifExample.sh

--- ifExample.sh
+++ ifExample.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 funcIf(){
-var=$1;
-if test "$var" -gt 10
-	then
-		echo "Greater than 10"
-elif test "$var" -eq 10
-	then
-		echo "Equal"
-else
-		echo "Less than 10"
-fi
+	var=$1;
+	if test "$var" -gt 10
+		then
+			echo "Greater than 10"
+	elif test "$var" -eq 10
+		then
+			echo "Equal"
+	else
+			echo "Less than 10"
+	fi
 }
 
 funcIf $1
