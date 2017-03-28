7d32afec9417d138a22cec4e59b9b8faca843985
Newton Galiza <newtonjgaliza@gmail.com>
2017-03-28 15:16:07 -0300

    Create ifExample.sh

---
+++ ifExample.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+funcIf(){
+var=$1;
+if test "$var" -gt 10
+	then
+		echo "Greater than 10"
+elif test "$var" -eq 10
+	then
+		echo "Equal"
+else
+		echo "Less than 10"
+fi
+}
+
+funcIf $1
+
+
+:<<'run'
+  ./ifExample.sh <number>
+run
